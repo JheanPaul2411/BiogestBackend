0ea03bf7e2579ad2a26a0841cafd7ca49f11631a
Richard Guach <guachrichard@gmail.com>
2024-01-08 16:30:30 -0500

    Solucionado errores

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,47 +1,50 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/Register.dto';
 import { hash, compare } from 'bcrypt';
 import { LoginDTO } from './dto/Login.dto';
-import {JwtService} from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
     constructor(
         private readonly userService: UsersService,
-        private readonly jwtAuthService:JwtService
+        private readonly jwtAuthService: JwtService
     ) { }
 
     async login(loginDto: LoginDTO) {
-    const { email, password } = loginDto;
-    const findUSer = await this.userService.findUserByEmail(email);
-        console.log("Email",email)
-    if (!findUSer) throw new HttpException('El usuario no se encontró', 404);
+        const { email, password } = loginDto;
+        const findUSer = await this.userService.findUserByEmail(email);
+        console.log("Email", email)
+        if (!findUSer) throw new HttpException('El usuario no se encontró', 404);
 
-    const checkPassword = await compare(password, findUSer.password);
+        const checkPassword = await compare(password, findUSer.password);
 
-    console.log(password)
-    console.log(findUSer)
-    if (!checkPassword) throw new HttpException('La contraseña es incorrecta', HttpStatus.FORBIDDEN);
+        console.log(password)
+        console.log(findUSer)
+        if (!checkPassword) throw new HttpException('La contraseña es incorrecta', HttpStatus.FORBIDDEN);
 
-    const payload={id:findUSer.id, name:findUSer.nombre}
-    const token = await this.jwtAuthService.sign(payload)
+        const payload = { id: findUSer.id, name: findUSer.nombre }
+        const token = await this.jwtAuthService.sign(payload)
 
-    const userCopy = { ...findUSer };
+        const userCopy = { ...findUSer };
 
-    delete userCopy.password;
+        delete userCopy.password;
 
-    const data = {
-        user: userCopy,
-        token
-    };
-    return data;
-}
+        const data = {
+            user: userCopy,
+            token
+        };
+        return data;
+    }
 
 
     async register(registerDto: RegisterDto) {
         const { password } = registerDto;
+        if (!password) {
+            throw new BadRequestException('La contraseña no puede estar vacía');
+        }
         const passwordHashed = await hash(password, 10);
         registerDto = { ...registerDto, password: passwordHashed };
 
