0e08d184bed5163d11134d8b9a32a8b921a20dcb
Richard Guach <guachrichard@gmail.com>
2024-01-19 20:04:15 -0500

    Arreglado el método de validacion del token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
-import { Module  } from '@nestjs/common';
+import {  Module  } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import {  } from './auth/auth.middleware';
 
 @Module({
   imports: [PrismaModule, UsersModule, AuthModule, ConfigModule.forRoot(),
   ],
   controllers: [],
 })
-export class AppModule { }
+export class AppModule  {
+  /*
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('login')
+  }
+  */
+}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,12 @@
-import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/Register.dto';
 import { LoginDTO } from './dto/Login.dto';
 import * as jwt from 'jsonwebtoken';
 
-
 @Controller('auth')
 export class AuthController {
-
-    constructor(private readonly authService: AuthService) { }
+    constructor(private readonly authService: AuthService) {}
 
     @Post('login')
     login(@Body() loginDto: LoginDTO) {
@@ -20,29 +18,33 @@ export class AuthController {
         return this.authService.register(registerDto);
     }
 
-    
-    
+
+
     /*
     !ERROR:
     Cuando envio el token que se almacena en el session storage del usario desde React, no funciona (me dice que el token no es válido).
     Sin embargo, cuando envío el mismo token desde POSTMAN, si funciona (me dice que el token si es válido).
     
     ?Ya verifiqué los CORS, y admití las peticiones desde React, pero sigue sin funcionar */
-    
+
+
     @Get('validate')
-    async validateToken(@Req() request) {
-        const token = request.headers.authorization;
+    async validateToken(@Req() request: any) {
+        let token: string = request.headers.authorization;
+        token = token.trim();
+
         console.log('Received token:', token);
 
         try {
-            jwt.verify(token, process.env.JWT_SECRET);
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
             console.log('Token verification successful');
+            console.log('Decoded token:', decodedToken);
             return { isValid: true };
         } catch (err) {
             console.error('Token verification failed:', err);
+            console.log('\n');
+            console.log(token);
             return { isValid: false };
         }
     }
-
-
 }
