9e985fc20efe8195fb4367a1f03c675db57465e2
Richard Guach <guachrichard@gmail.com>
2024-03-01 14:09:05 -0500

    Se agregaron seeders

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import { hash } from 'bcrypt';
+import { users } from './users';
+import { PrismaClient } from '@prisma/client'
+import { citas } from "./citas";
+const prisma = new PrismaClient();
+
+async function main() {
+    for (const user of users) {
+        const passwordHashed = await hash(user.password, 10);
+        const data = { ...user, password: passwordHashed };
+
+        await prisma.usuario.create({
+            data
+        })
+    }
+
+    for(const cita of citas){
+        await prisma.cita.create({data:cita})
+    }
+
+
+}
+
+main().catch(e => {
+    console.log(e);
+    process.exit(1);
+}).finally(() => {
+    prisma.$disconnect();
+});
